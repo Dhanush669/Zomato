9d7f6ec2cb4b675949fca91e8b735cafc0a1a962
Dhanush669 <dhanushrukmangadhan@gmail.com>
2020-11-21 00:21:49 +0530

    initial commit

--- app/src/main/java/com/example/zomato/RecyclerAdaptershop.java
+++ app/src/main/java/com/example/zomato/RecyclerAdaptershop.java
@@ -39,7 +39,7 @@ public class RecyclerAdaptershop extends RecyclerView.Adapter<RecyclerAdaptersho
         holder.loc.setText(datagetsets.get(position).getLoc());
         holder.rate.setText(datagetsets.get(position).getRate());
         Glide.with(context).load(datagetsets.get(position).getPhoto()).centerCrop()
-                .into(holder.imageView);
+                .into(holder.photo);
     }
 
     @Override
@@ -48,11 +48,11 @@ public class RecyclerAdaptershop extends RecyclerView.Adapter<RecyclerAdaptersho
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-        ImageView imageView;
+        ImageView photo;
         TextView name,loc,time,rate;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView=itemView.findViewById(R.id.near);
+            photo=itemView.findViewById(R.id.near);
             name=itemView.findViewById(R.id.shopname);
             loc=itemView.findViewById(R.id.shoploc);
             time=itemView.findViewById(R.id.shoptime);
