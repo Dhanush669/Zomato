ad16b5c671b0b3cc516119d4e8ed8f009e29d446
Dhanush669 <dhanushrukmangadhan@gmail.com>
2020-11-20 14:03:23 +0530

    initial commit

--- app/src/main/java/com/example/zomato/MainActivity.java
+++ app/src/main/java/com/example/zomato/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zomato;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,15 +10,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView,recyclerView1;
-    List<Datagetset> datagetsetList;
+    RecyclerView recyclerView,recyclerView1,off,off1,off2;
+    List<Datagetset> res1,res2;
+    List<Integer> offers,offers1,offers2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView=findViewById(R.id.ordering);
         recyclerView1=findViewById(R.id.ordering1);
-        datagetsetList=new ArrayList<>();
-       // Datagetset dgs=new Datagetset("cake",R.drawable.)
+        off=findViewById(R.id.offers);
+        off1=findViewById(R.id.offers1);
+        off2=findViewById(R.id.offers2);
+        res1=new ArrayList<>();
+        res2=new ArrayList<>();
+        Datagetset dgs=new Datagetset("cake",R.drawable.cake);
+        Datagetset dgs1=new Datagetset("noodles",R.drawable.noodels);
+        Datagetset dgs2=new Datagetset("paneer",R.drawable.panner);
+        res1.add(dgs);
+        res1.add(dgs1);
+        res1.add(dgs2);
+        setRecyclerView(res1,recyclerView);
+        Datagetset dgs3=new Datagetset("bbq",R.drawable.bbq);
+        Datagetset dgs4=new Datagetset("pizza",R.drawable.pizza);
+        Datagetset dgs5=new Datagetset("biriyani",R.drawable.biriyani);
+        res2.add(dgs3);
+        res2.add(dgs4);
+        res2.add(dgs5);
+        setRecyclerView(res2,recyclerView1);
+        offers=new ArrayList<>();
+        offers.add(R.drawable.alloffers);
+        offers.add(R.drawable.pocket);
+        offers.add(R.drawable.nonveg);
+        setOfferView(offers,off);
+        offers1=new ArrayList<>();
+        offers1.add(R.drawable.pureveg);
+        offers1.add(R.drawable.trending);
+        offers1.add(R.drawable.hygiene);
+        setOfferView(offers1,off1);
+        offers2=new ArrayList<>();
+        offers2.add(R.drawable.meal);
+        offers2.add(R.drawable.offers);
+        offers2.add(R.drawable.zomotapro);
+        setOfferView(offers2,off2);
+    }
+
+    public void setRecyclerView(List<Datagetset> list,RecyclerView rv){
+        RecyclerAdapter ra=new RecyclerAdapter(list,MainActivity.this);
+        rv.setAdapter(ra);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        rv.setLayoutManager(linearLayoutManager);
+    }
+    public void setOfferView(List<Integer> list,RecyclerView rv){
+        RecyclerAdapter1 ra=new RecyclerAdapter1(list,MainActivity.this);
+        rv.setAdapter(ra);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        rv.setLayoutManager(linearLayoutManager);
     }
 }
--- app/src/main/java/com/example/zomato/RecyclerAdapter1.java
+++ app/src/main/java/com/example/zomato/RecyclerAdapter1.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,12 +15,12 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>{
-    List<Datagetset> datagetsets=new ArrayList<>();
+public class RecyclerAdapter1 extends RecyclerView.Adapter<RecyclerAdapter1.MyViewHolder>{
+    List<Integer> offersph=new ArrayList<>();
     Context context;
 
-    public RecyclerAdapter(List<Datagetset> datagetsets, Context context) {
-        this.datagetsets = datagetsets;
+    public RecyclerAdapter1(List<Integer> datagetsets, Context context) {
+        this.offersph = datagetsets;
         this.context = context;
     }
 
@@ -28,30 +28,28 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
-        view= LayoutInflater.from(parent.getContext()).inflate(R.layout.holder,parent,false);
+        view= LayoutInflater.from(parent.getContext()).inflate(R.layout.offersholder,parent,false);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Glide.with(context).load(datagetsets.get(position).getPhoto()).centerCrop()
-
+        Glide.with(context).load(offersph.get(position)).centerCrop()
                 .into(holder.imageView);
-        holder.name.setText(datagetsets.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return datagetsets.size();
+        return offersph.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         ImageView imageView;
-        TextView name;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView=itemView.findViewById(R.id.nei);
-            name=itemView.findViewById(R.id.itemname);
+            imageView=itemView.findViewById(R.id.offersphoto);
+
         }
     }
 }
+
